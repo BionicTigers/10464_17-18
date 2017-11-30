05cd65cec5c4c8d279b32391a9f4f7f4f9256eec
Nathanous Trawets <natedawg1121@gmail.com>
2017-11-29 19:06:06 -0500

    nathan did not touch gradle

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryTesting.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@TeleOp(name="Mechanum Protobot Tank", group="Protobot")
+
+public class TelemetryTesting {
+
+    private Orientation angles;
+    private BNO055IMU imu;
+
+    public void init() {
+        BNO055IMU imu;
+    }
+
+}
