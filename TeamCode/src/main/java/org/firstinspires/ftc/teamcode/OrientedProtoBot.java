8afdacd3d5154626ecede78a74a8be168e46a83b
Emily Kiehl <emilydkiehl@gmail.com>
2017-12-02 23:13:18 -0500

    Case order good

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OrientedProtoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OrientedProtoBot.java
@@ -43,8 +43,7 @@ public abstract class OrientedProtoBot extends OpMode {
     String formatAngle;
 
 
-    public class SensorBNO055IMU extends LinearOpMode
-    {
+    public class OrientedProtobot extends LinearOpMode {
         //----------------------------------------------------------------------------------------------
         // State
         //----------------------------------------------------------------------------------------------
@@ -60,17 +59,18 @@ public abstract class OrientedProtoBot extends OpMode {
         // Main logic
         //----------------------------------------------------------------------------------------------
 
-        @Override public void runOpMode() {
+        @Override
+        public void runOpMode() {
 
             // Set up the parameters with which we will use our IMU. Note that integration
             // algorithm here just reports accelerations to the logcat log; it doesn't actually
             // provide positional information.
             BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-            parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-            parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+            parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+            parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
             parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
-            parameters.loggingEnabled      = true;
-            parameters.loggingTag          = "IMU";
+            parameters.loggingEnabled = true;
+            parameters.loggingTag = "IMU";
             parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
             // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
@@ -102,57 +102,65 @@ public abstract class OrientedProtoBot extends OpMode {
 
             // At the beginning of each telemetry update, grab a bunch of data
             // from the IMU that we will then display in separate lines.
-            telemetry.addAction(new Runnable() { @Override public void run()
-            {
-                // Acquiring the angles is relatively expensive; we don't want
-                // to do that in each of the three items that need that info, as that's
-                // three times the necessary expense.
-                angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-                gravity  = imu.getGravity();
-            }
+            telemetry.addAction(new Runnable() {
+                @Override
+                public void run() {
+                    // Acquiring the angles is relatively expensive; we don't want
+                    // to do that in each of the three items that need that info, as that's
+                    // three times the necessary expense.
+                    angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                    gravity = imu.getGravity();
+                }
             });
 
             telemetry.addLine()
                     .addData("status", new Func<String>() {
-                        @Override public String value() {
+                        @Override
+                        public String value() {
                             return imu.getSystemStatus().toShortString();
                         }
                     })
                     .addData("calib", new Func<String>() {
-                        @Override public String value() {
+                        @Override
+                        public String value() {
                             return imu.getCalibrationStatus().toString();
                         }
                     });
 
             telemetry.addLine()
                     .addData("heading", new Func<String>() {
-                        @Override public String value() {
+                        @Override
+                        public String value() {
                             return formatAngle(angles.angleUnit, angles.firstAngle);
                         }
                     })
                     .addData("roll", new Func<String>() {
-                        @Override public String value() {
+                        @Override
+                        public String value() {
                             return formatAngle(angles.angleUnit, angles.secondAngle);
                         }
                     })
                     .addData("pitch", new Func<String>() {
-                        @Override public String value() {
+                        @Override
+                        public String value() {
                             return formatAngle(angles.angleUnit, angles.thirdAngle);
                         }
                     });
 
             telemetry.addLine()
                     .addData("grvty", new Func<String>() {
-                        @Override public String value() {
+                        @Override
+                        public String value() {
                             return gravity.toString();
                         }
                     })
                     .addData("mag", new Func<String>() {
-                        @Override public String value() {
+                        @Override
+                        public String value() {
                             return String.format(Locale.getDefault(), "%.3f",
-                                    Math.sqrt(gravity.xAccel*gravity.xAccel
-                                            + gravity.yAccel*gravity.yAccel
-                                            + gravity.zAccel*gravity.zAccel));
+                                    Math.sqrt(gravity.xAccel * gravity.xAccel
+                                            + gravity.yAccel * gravity.yAccel
+                                            + gravity.zAccel * gravity.zAccel));
                         }
                     });
         }
@@ -165,146 +173,141 @@ public abstract class OrientedProtoBot extends OpMode {
             return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
         }
 
-        String formatDegrees(double degrees){
+        String formatDegrees(double degrees) {
             return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
         }
-    }
-    public void loop()
-    {
-        ////////////////
-        // MAIN DRIVE //
-        ////////////////
 
-        double r = Math.hypot(-gamepad1.right_stick_x, -gamepad1.left_stick_y);
-        double robotAngle = Math.atan2(-gamepad1.right_stick_x, -gamepad1.left_stick_y) - Math.PI / 4;
-        double rightX = gamepad1.left_stick_x;
-        final double v1 = r * Math.sin(robotAngle) + rightX;
-        final double v2 = r * Math.cos(robotAngle) + rightX;
-        final double v3 = r * Math.cos(robotAngle) - rightX;
-        final double v4 = r * Math.sin(robotAngle) - rightX;
+        public void loop() {
+            ////////////////
+            // MAIN DRIVE //
+            ////////////////
+            composeTelemetry();
+            double r = Math.hypot(-gamepad1.right_stick_x, -gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(-gamepad1.right_stick_x, -gamepad1.left_stick_y) - Math.PI / 4;
+            double rightX = gamepad1.left_stick_x;
+            final double v1 = r * Math.sin(robotAngle) + rightX;
+            final double v2 = r * Math.cos(robotAngle) + rightX;
+            final double v3 = r * Math.cos(robotAngle) - rightX;
+            final double v4 = r * Math.sin(robotAngle) - rightX;
 
-        motorFrontRight.setPower(v1);
-        motorFrontLeft.setPower(v2);
-        motorBackRight.setPower(v3);
-        motorBackLeft.setPower(v4);
-
-        /////////////////////////////
-        // ORIENTATION CALIBRATION //
-        /////////////////////////////
-
-
-        if (gamepad1.a) {
-            telemetry.addData("angles", angles.firstAngle);
-            telemetry.addData("imu gyro calib status", imu.getCalibrationStatus());
-            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-            double P = -((Math.abs(gamepad1.left_stick_y) + Math.abs(gamepad1.left_stick_x) / 2));
-            double H = (angles.firstAngle * Math.PI) / 180;
-            double Ht = (Math.PI + Math.atan2(gamepad1.left_stick_x, gamepad1.left_stick_y));
-            motorBackRight.setPower(P * Math.sin(H - Ht));
-            motorFrontLeft.setPower(P * Math.sin(H - Ht));
-            motorBackLeft.setPower(P * Math.cos(H - Ht));
-            motorFrontRight.setPower(P * Math.cos(H - Ht));
-
-        }
-        else {
-            telemetry.addData("imu gyro calib status", imu.getCalibrationStatus());
             motorFrontRight.setPower(v1);
             motorFrontLeft.setPower(v2);
             motorBackRight.setPower(v3);
             motorBackLeft.setPower(v4);
-        }
+
+            /////////////////////////////
+            // ORIENTATION CALIBRATION //
+            /////////////////////////////
 
 
+            if (gamepad1.a) {
+                telemetry.addData("angles", angles.firstAngle);
+                telemetry.addData("imu gyro calib status", imu.getCalibrationStatus());
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                double P = -((Math.abs(gamepad1.left_stick_y) + Math.abs(gamepad1.left_stick_x) / 2));
+                double H = (angles.firstAngle * Math.PI) / 180;
+                double Ht = (Math.PI + Math.atan2(gamepad1.left_stick_x, gamepad1.left_stick_y));
+                motorBackRight.setPower(P * Math.sin(H - Ht));
+                motorFrontLeft.setPower(P * Math.sin(H - Ht));
+                motorBackLeft.setPower(P * Math.cos(H - Ht));
+                motorFrontRight.setPower(P * Math.cos(H - Ht));
 
-        //if (gamepad1.left_stick_button) {
-        //angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-
-        //telemetry.addData("heading", angles);
-
-        //final double v5 = r * Math.sin(robotAngle) + rightX + angles.firstAngle;
-        //final double v6 = r * Math.cos(robotAngle) + rightX + angles.firstAngle;
-        //final double v7 = r * Math.cos(robotAngle) - rightX + angles.firstAngle;
-        //final double v8 = r * Math.sin(robotAngle) - rightX + angles.firstAngle;
-
-        //motorFrontRight.setPower(v5);
-        //motorFrontLeft.setPower(v6);
-        //motorBackRight.setPower(v7);
-        //motorBackLeft.setPower(v8);
-
-        //}
-
-        ///////////////////////
-        // COLLECTION SERVOS //
-        ///////////////////////
-
-        if (gamepad2.x) {
-            if (left < 0.3 && right > 0.32) {
-                left += .01;
-                right -= .01;
+            } else {
+                telemetry.addData("imu gyro calib status", imu.getCalibrationStatus());
+                motorFrontRight.setPower(v1);
+                motorFrontLeft.setPower(v2);
+                motorBackRight.setPower(v3);
+                motorBackLeft.setPower(v4);
             }
-            franny.setPosition(left);
-            mobert.setPosition(right);
-        }
-        else if (gamepad2.b) {
-            if (left > 0.00 && right < 1.0) {
-                left -= .01;
-                right += .01;
+
+
+            //if (gamepad1.left_stick_button) {
+            //angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+            //telemetry.addData("heading", angles);
+
+            //final double v5 = r * Math.sin(robotAngle) + rightX + angles.firstAngle;
+            //final double v6 = r * Math.cos(robotAngle) + rightX + angles.firstAngle;
+            //final double v7 = r * Math.cos(robotAngle) - rightX + angles.firstAngle;
+            //final double v8 = r * Math.sin(robotAngle) - rightX + angles.firstAngle;
+
+            //motorFrontRight.setPower(v5);
+            //motorFrontLeft.setPower(v6);
+            //motorBackRight.setPower(v7);
+            //motorBackLeft.setPower(v8);
+
+            //}
+
+            ///////////////////////
+            // COLLECTION SERVOS //
+            ///////////////////////
+
+            if (gamepad2.x) {
+                if (left < 0.3 && right > 0.32) {
+                    left += .01;
+                    right -= .01;
+                }
+                franny.setPosition(left);
+                mobert.setPosition(right);
+            } else if (gamepad2.b) {
+                if (left > 0.00 && right < 1.0) {
+                    left -= .01;
+                    right += .01;
+                }
+                franny.setPosition(left);
+                mobert.setPosition(right);
             }
-            franny.setPosition(left);
-            mobert.setPosition(right);
-        }
 
-        if (gamepad2.left_bumper) {
-            if (left < 0.3) {
-                left += .01;
+            if (gamepad2.left_bumper) {
+                if (left < 0.3) {
+                    left += .01;
+                }
+                franny.setPosition(left);
+            } else if (gamepad2.left_trigger > .7) {
+                if (left > 0.0) {
+                    left -= .01;
+                }
+                franny.setPosition(left);
             }
-            franny.setPosition(left);
-        }
-        else if (gamepad2.left_trigger > .7) {
-            if (left > 0.0) {
-                left -= .01;
+
+            if (gamepad2.right_bumper) {
+                if (right > 0.32) {
+                    right -= .01;
+                }
+                mobert.setPosition(right);
+            } else if (gamepad2.right_trigger > .7) {
+                if (right < 1) {
+                    right += .01;
+                }
+                mobert.setPosition(right);
             }
-            franny.setPosition(left);
-        }
 
-        if (gamepad2.right_bumper) {
-            if (right > 0.32) {
-                right -= .01;
+            telemetry.addData("Left", left);
+            telemetry.addData("Right", right);
+            telemetry.addData("franny", franny);
+            telemetry.addData("mobert", mobert);
+            telemetry.addData("angles", angles.firstAngle);
+
+
+            ///////////////////
+            // BELT CONTROLS //
+            ///////////////////
+
+            if (gamepad2.dpad_up) {
+                top.setPower(-0.45);
+            } else if (gamepad2.dpad_down) {
+                top.setPower(0.45);
+            } else {
+                top.setPower(0);
             }
-            mobert.setPosition(right);
-        }
-        else if (gamepad2.right_trigger > .7) {
-            if (right < 1) {
-                right += .01;
+
+            if (gamepad2.y) {
+                front.setPower(-0.7);
+            } else if (gamepad2.a) {
+                front.setPower(0.7);
+            } else {
+                front.setPower(0);
             }
-            mobert.setPosition(right);
-        }
-
-        telemetry.addData("Left", left);
-        telemetry.addData("Right", right);
-        telemetry.addData("franny", franny);
-        telemetry.addData("mobert", mobert);
-        telemetry.addData("angles", angles.firstAngle);
-
-
-        ///////////////////
-        // BELT CONTROLS //
-        ///////////////////
-
-        if (gamepad2.dpad_up) {
-            top.setPower(-0.45);
-        } else if (gamepad2.dpad_down) {
-            top.setPower(0.45);
-        } else {
-            top.setPower(0);
-        }
-
-        if (gamepad2.y) {
-            front.setPower(-0.7);
-        } else if (gamepad2.a) {
-            front.setPower(0.7);
-        } else {
-            front.setPower(0);
         }
     }
 }
