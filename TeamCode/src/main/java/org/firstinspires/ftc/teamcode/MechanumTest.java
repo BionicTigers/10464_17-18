2a49f85ae22bad28e3085556ce27d9947bf9414d
Nathanous Trawets <natedawg1121@gmail.com>
2017-12-21 14:06:52 -0500

    nathan did not touch gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumTest.java
@@ -43,7 +43,7 @@ public void loop() {
         calibToggle = 1;
     }
 
-    if (gamepad1.b) {
+    if (gamepad1.x) {
         calibToggle = 2;
     }
 
@@ -51,13 +51,13 @@ public void loop() {
         calibToggle = 3;
     }
 
-    if (gamepad1.x) {
+    if (gamepad1.b) {
         calibToggle = 4;
     }
 
     if (calibToggle == 1) {
-        double P = -(Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y));
-        double robotAngle = -(Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x));
+        double P = Math.hypot(-gamepad1.left_stick_x, -gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(-gamepad1.left_stick_y, -gamepad1.left_stick_x);
         double rightX = -gamepad1.right_stick_x;
         double sinRAngle = Math.sin(robotAngle);
         double cosRAngle = Math.cos(robotAngle);
@@ -74,8 +74,8 @@ public void loop() {
     }
 
     if (calibToggle == 2) {
-        double P = -(Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y));
-        double robotAngle = -(Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x));
+        double P = Math.hypot(-gamepad1.left_stick_x, -gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(-gamepad1.left_stick_y, -gamepad1.left_stick_x);
         double rightX = -gamepad1.right_stick_x;
         double sinRAngle = Math.sin(robotAngle);
         double cosRAngle = Math.cos(robotAngle);
@@ -92,8 +92,8 @@ public void loop() {
     }
 
     if (calibToggle == 3) {
-        double P = -(Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y));
-        double robotAngle = -(Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x));
+        double P = Math.hypot(-gamepad1.left_stick_x, -gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(-gamepad1.left_stick_y, -gamepad1.left_stick_x);
         double rightX = -gamepad1.right_stick_x;
         double sinRAngle = Math.sin(robotAngle);
         double cosRAngle = Math.cos(robotAngle);
@@ -110,8 +110,8 @@ public void loop() {
     }
 
     if (calibToggle == 4) {
-        double P = -(Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y));
-        double robotAngle = -(Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x));
+        double P = Math.hypot(-gamepad1.left_stick_x, -gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(-gamepad1.left_stick_y, -gamepad1.left_stick_x);
         double rightX = -gamepad1.right_stick_x;
         double sinRAngle = Math.sin(robotAngle);
         double cosRAngle = Math.cos(robotAngle);
